8de700a9182f8da54d59da9a9ccb2a5dbe4bc4bb
saif braham <saif.braham@gmail.com>
2024-11-23 10:26:26 +0100

    Migrate TexttestFixture main class to @Test method

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,10 +1,28 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
+
+    private GildedRose app;
+    private Item[] items;
+    public static final int ARGS =10;
+
+    @BeforeEach
+    void setUp() {
+        items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20),
+            new Item("Aged Brie", 2, 0),
+            new Item("Elixir of the Mongoose", 5, 7),
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Conjured Mana Cake", 3, 6)
+        };
+        app = new GildedRose(items);
+    }
 
     @Test
     void foo() {
@@ -12,6 +30,20 @@ class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
+    }
+
+    @Test
+    void textTestFixture(){
+        int days = ARGS;
+        for (int i = 0; i < days; i++) {
+            System.out.println("-------- day " + i + " --------");
+            System.out.println("name, sellIn, quality");
+            for (Item item : items) {
+                System.out.println(item);
+            }
+            System.out.println();
+            app.updateQuality();
+        }
     }
 
 }
