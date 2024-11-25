8d5b9289de7122dacc93b2ae26b19e5992d2b0d1
saif braham <saif.braham@gmail.com>
2024-11-25 16:57:45 +0100

    Use Configuration Files for Item Registration - add items.properties to create new special items - apdate UpdatableItemFactory class to make the app even more dynamic and maintainable

--- src/main/java/com/gildedrose/model/UpdatableItemFactory.java
+++ src/main/java/com/gildedrose/model/UpdatableItemFactory.java
@@ -1,16 +1,47 @@
 package com.gildedrose.model;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.function.Function;
 
 public class UpdatableItemFactory {
     private static final Map<String, Function<Item, UpdatableItem>> ITEM_CREATORS = new HashMap<>();
 
     static {
-        ITEM_CREATORS.put("Aged Brie", AgedBrie::new);
-        ITEM_CREATORS.put("Backstage passes to a TAFKAL80ETC concert", BackstagePass::new);
-        ITEM_CREATORS.put("Sulfuras, Hand of Ragnaros", Sulfuras::new);
+        loadItemsFromConfig();
+    }
+
+    private static void loadItemsFromConfig() {
+        try (InputStream input = UpdatableItemFactory.class.getClassLoader().getResourceAsStream("items.properties")) {
+            if (input == null) {
+                throw new RuntimeException("items.properties file not found in resources folder.");
+            }
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+            for (String itemName : prop.stringPropertyNames()) {
+                String className = prop.getProperty(itemName);
+                registerItem(itemName, item -> instantiateItem(className, item));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load item configuration", e);
+        }
+    }
+
+    private static UpdatableItem instantiateItem(String className, Item item) {
+        try {
+            Class<?> clazz = Class.forName(className);
+            return (UpdatableItem) clazz.getConstructor(Item.class).newInstance(item);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create item instance for class: " + className, e);
+        }
+    }
+
+    public static void registerItem(String itemName, Function<Item, UpdatableItem> constructor) {
+        ITEM_CREATORS.put(itemName, constructor);
     }
 
     public static UpdatableItem createUpdatableItem(Item item) {
