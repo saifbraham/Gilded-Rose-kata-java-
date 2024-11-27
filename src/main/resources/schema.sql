f837f054a8edcf7f06a0b258f9b1388508712fdc
saif braham <saif.braham@gmail.com>
2024-11-27 11:46:13 +0100

    create GildedRoseController create Adapter JsonItem class to convert legacy Item class update test class integration create .http file for real test

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS SPECIAL_ITEM (
-    name VARCHAR(255) PRIMARY KEY,
-    class_name VARCHAR(255)
-    );
+    name VARCHAR(255) NOT NULL,
+    class_name VARCHAR(255) NOT NULL
+);
+
--- src/test/java/com/gildedrose/GildedRoseApiIntegrationTest.java
+++ src/test/java/com/gildedrose/GildedRoseApiIntegrationTest.java
@@ -2,15 +2,12 @@ package com.gildedrose;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gildedrose.adapter.JsonItem;
-import com.gildedrose.model.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-
 import java.util.List;
 
 
